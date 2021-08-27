76f5388b097643761d1d5a5d6bd4e925f6ac7109
wbrunovieira <wbrunovieira77@gmail.com>
2021-08-27 20:56:52 +0100

    project to list a repo in github api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-export function App(){
-    return(
-        <h1>Hello World</h1>
-    )
-}
+import RepositoryList from './components/RepositoryList';
+import './styles/global.scss';
+
+const App = () => <RepositoryList />;
+
+export default App;
